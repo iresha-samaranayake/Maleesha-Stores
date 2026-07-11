b3b2706ab26db93bc0a66067666450bf74ff869a
iresha1234-afk <ireshasamaranayake104@gmail.com>
2026-07-11 22:28:54 +0530

    Update CartContext.jsx

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -65,6 +65,7 @@ export const CartProvider = ({ children }) => {
     <CartContext.Provider
       value={{
         cart,
+        cartItems: cart,
         addToCart,
         removeFromCart,
         updateQuantity,
@@ -76,4 +77,12 @@ export const CartProvider = ({ children }) => {
       {children}
     </CartContext.Provider>
   );
+};
+
+export const useCart = () => {
+  const context = React.useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
 };
