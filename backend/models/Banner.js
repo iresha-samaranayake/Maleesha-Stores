7ae7e46ab6e37701776249b3ffed9e817e69219d
iresha1234-afk <ireshasamaranayake104@gmail.com>
2026-07-13 21:34:24 +0530

    Create Banner.js

---
+++ backend/models/Banner.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const BannerSchema = new mongoose.Schema({
+  image_url: {
+    type: String,
+    required: [true, 'Please add a banner image URL']
+  },
+  type: {
+    type: String,
+    enum: ['Main Carousel', 'Small Promo'],
+    required: [true, 'Please specify the banner type']
+  },
+  target_link: {
+    type: String,
+    default: ''
+  },
+  is_active: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+});
+
+// Configure virtual field 'id' to map from '_id'
+BannerSchema.virtual('id').get(function() {
+  return this._id.toHexString();
+});
+
+// Ensure virtual fields are serialized
+BannerSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+  }
+});
+
+BannerSchema.set('toObject', {
+  virtuals: true
+});
+
+module.exports = mongoose.model('Banner', BannerSchema);
