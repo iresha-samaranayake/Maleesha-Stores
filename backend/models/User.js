beb9ec1b00f2aec57fe47a16f15a180f4dee2d62
iresha1234-afk <ireshasamaranayake104@gmail.com>
2026-07-16 18:54:03 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,6 +29,12 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: '',
     },
+    favorites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+      }
+    ],
   },
   {
     timestamps: true,
